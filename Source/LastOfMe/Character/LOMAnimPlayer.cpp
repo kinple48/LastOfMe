f89cf3f978871c1c74e8145b2fb549f8a969f107
SangAh-gif <tkddk0517@naver.com>
2025-02-10 23:06:35 +0900

    AnimPlayer

---
+++ Source/LastOfMe/Character/LOMAnimPlayer.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LOMAnimPlayer.h"
+#include "MainPlayerCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+ULOMAnimPlayer::ULOMAnimPlayer()
+{
+}
+
+void ULOMAnimPlayer::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+
+	Player = Cast<AMainPlayerCharacter>(TryGetPawnOwner());
+}
+
+void ULOMAnimPlayer::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (Player == nullptr)
+		return;
+	
+	Speed = Player->GetVelocity().Size2D();
+}
